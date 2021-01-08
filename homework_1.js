47aa8167e3e26cd19829f0c63456aee6a031658f
ElwydErni <76775000+ElwydErni@users.noreply.github.com>
2021-01-08 19:46:21 +0300

    Add files via upload

---
+++ homework_1.js
@@ -0,0 +1,5 @@
+let namegenerator = 'Генератор защитного поля';
+let price = 1000;
+console.log('Выбран ' + namegenerator + ' цeна ' + price);
+price = 2000;
+console.log('Выбран ' + namegenerator + ' цeна ' + price);
